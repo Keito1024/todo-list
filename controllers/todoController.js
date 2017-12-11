1528b119385dbda325277e274e7c0444feba3f3b
勝田圭豊 <kkrt0724@gmail.com>
2017-12-11 13:22:47 +0900

    todolist date add

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -6,51 +6,56 @@ mongoose.connect('mongodb://test:test@ds113702.mlab.com:13702/todo');
 
 //Create a schema - this is like a blueprint
 var todoSchema = new mongoose.Schema({
-  item: String
+  item: String,
+  limitDate: Date
 });
 
 var Todo = mongoose.model('Todo', todoSchema);
+var urlencodedParser = bodyParser.urlencoded({
+  extended: false
+});
 
-//var data = [{item: 'get milk'},{item: 'walk dog'},{item: 'kick some codong ass'}];
-var urlencodedParser = bodyParser.urlencoded({extended: false});
-
-module.exports = function(app){
-
-
+module.exports = function(app) {
 
 
-// 入力情報を取得
-app.get('/todo', function(req, res){
-  //get data from mongodb and pass it to view
-  Todo.find({},function(err,data){
-    if (err) throw err;
-    res.render('todo',{todos: data});
+
+
+  // 入力情報を取得
+  app.get('/todo', function(req, res) {
+    //get data from mongodb and pass it to view
+    Todo.find({}, function(err, data) {
+      if (err) throw err;
+      res.render('todo', {
+        todos: data
+      });
+    });
+
   });
 
-});
 
 
 
-
-// 投稿機能
-app.post('/todo', urlencodedParser,function(req, res){
-  //get data from view and add it to mongodb
-  var newTodo = Todo(req.body).save(function(err,data){
-    if (err) throw err;
-    res.json(data);
-  })
-});
-
-
-
-
-// 削除機能
-app.delete('/todo/:item', function(req, res){
-  //delete the requested item from mongodb
-  Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err,data){
-    if (err) throw err;
-    res.json(data);
+  // 投稿機能
+  app.post('/todo', urlencodedParser, function(req, res) {
+    //get data from view and add it to mongodb
+    var newTodo = Todo(req.body).save(function(err, data) {
+      if (err) throw err;
+      res.json(data);
+    })
   });
-});
+
+
+
+
+  // 削除機能
+  app.delete('/todo/:item', function(req, res) {
+    //delete the requested item from mongodb
+    Todo.find({
+      item: req.params.item.replace(/\-/g, " ")
+    }).remove(function(err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
+  });
 
 };
